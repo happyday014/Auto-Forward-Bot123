4a41af4d6b1926365c183bb154c4fa49f5da6f1d
happyday014 <103929071+happyday014@users.noreply.github.com>
2022-04-20 14:08:50 +0000

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
 
       # Start text
-      START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @Ns_bot_updates, @Ns_Bot_supporters"             
+      START = "Hi I am a channel auto forward bot clone of <a href='https://t.me/Raj_Hack_Forwarder_bot'>Channel Automatic Raj bot</a>. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @Raj_Files_backup, @Raj_Files_chats, t.me/+rAu0_UMyiOBmNmE1"             
 
       #About text
       ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-<b>📢 Channel:</b> NS BOT UPDATES
-<b>👥 Group:</b> Ns BOT SUPPORT
+<b>👨‍💻 Developer:</b> @Mr_RAJx
+<b>📢 Channel:</b> @Raj_Files_backup
+<b>👥 Group:</b> @Raj_Files_chats
 <b>🌐Source Code:</b> Press Me 🥰"""
